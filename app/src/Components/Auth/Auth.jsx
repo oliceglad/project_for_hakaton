791651a7f442f56dc243896de8bc772f396d43ce
Вячеслав Чучманов <vaceslavcucmanov@MacBook-Air-Vaceslav.local>
2022-10-29 07:28:29 +0400

    nothing

--- app/src/Components/Auth/Auth.jsx
+++ app/src/Components/Auth/Auth.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import a from './Auth.module.scss';
 
 export const Auth = () => {
+    const [title, setTitle] = useState('')
+
+
+    function handleSubmit(e) {
+        console.log(title);
+    }
+
     return (
-        <form className={a.auth__form}>
+        <form className={a.auth__form} onSubmit={handleSubmit} >
             <input
                 placeholder="Логин"
                 className={a.auth__form__input}
                 type="text"
-                required
+                value={title}
+                onChange={event => setTitle(event.target.value)}
             />
             <input
                 placeholder="Пароль"
                 className={a.auth__form__input}
                 type="text"
-                required
             />
-            <button type="submit" className={a.auth__form__button}>
+            <button type="submit" className={a.auth__form__button} onClick={handleSubmit}>
                 Войти
             </button>
         </form>
