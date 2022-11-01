46c9295e7bb0548bcbb2409dbc3b953f19293589
OlicGlad <vycheslavofficial@gmail.com>
2022-11-01 22:31:16 +0400

    added styling is perfect

--- app/src/App.js
+++ app/src/App.js
@@ -3,63 +3,15 @@ import './App.css';
 import { Header } from './Components/Header/Header';
 import { Main } from './Components/Mainpage/Main';
 import a from '././/Components/Auth/Auth.module.scss';
+import { Auth } from './Components/Auth/Auth';
 
 function App() {
-  const [title, setTitle] = useState('')
-  const [titleTwo, setTitleNew] = useState('')
-  const [isAuth, setIsAuth] = useState(false)
-
-  function addToStorage(isAuth) {
-    localStorage.setItem('isAuth', isAuth);
-  }
-
-  useEffect(() => {
-    const item = localStorage.getItem('isAuth');
-    if (item) {
-      setIsAuth(item)
-    }
-  }, [])
-
-
-  const dictions = {
-    login: 'admin',
-    password: '123'
-  }
-
-  function handleSubmit() {
-
-    if (title === dictions.login && titleTwo === dictions.password) {
-      setIsAuth(true) 
-    }
-    
-    addToStorage()
-    return isAuth
-  }
-
-  if (!isAuth) {
+  if (!localStorage.getItem('isAuth')) {
     return (
       <div className='container'>
         <Header />
         <div className='main'>
-          <form className={a.auth__form}>
-            <input
-              placeholder="Логин"
-              className={a.auth__form__input}
-              type="text"
-              value={title}
-              onChange={event => setTitle(event.target.value)}
-            />
-            <input
-              placeholder="Пароль"
-              className={a.auth__form__input}
-              type="password"
-              value={titleTwo}
-              onChange={event => setTitleNew(event.target.value)}
-            />
-            <button type="submit" className={a.auth__form__button} onClick={handleSubmit}>
-              Войти
-            </button>
-          </form>
+          <Auth/>
         </div>
       </div>
 
--- app/src/Components/Auth/Auth.jsx
+++ app/src/Components/Auth/Auth.jsx
@@ -1,43 +1,53 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import a from './Auth.module.scss';
 
-export const Auth = ({isAuth}) => {
+export const Auth = () => {
     const [title, setTitle] = useState('')
     const [titleTwo, setTitleNew] = useState('')
+    const [isAuth, setIsAuth] = useState(false)
 
-    function handleSubmit(e) {
-
-        const dictions = {
-            login: "admin",
-            password: "123"
-        }
-
-        if (title == dictions.login && titleTwo === dictions.password) {
-            isAuth = true;
-        }
-
-        return isAuth
+    useEffect(() => {
+        setIsAuth(localStorage.getItem('isAuth'))
+      }, [])
+    
+    function addToStorage(isAuth) {
+        localStorage.setItem('isAuth', isAuth);
     }
 
+    const dictions = {
+        login: 'admin',
+        password: '123'
+      }
+    
+      function handleSubmit() {
+    
+        if (title === dictions.login && titleTwo === dictions.password) {
+          setIsAuth(true) 
+        }
+        
+        addToStorage()
+        return isAuth
+      }
+
     return (
-        <form className={a.auth__form} onSubmit={handleSubmit} >
-            <input
-                placeholder="Логин"
-                className={a.auth__form__input}
-                type="text"
-                value={title}
-                onChange={event => setTitle(event.target.value)}
-            />
-            <input
-                placeholder="Пароль"
-                className={a.auth__form__input}
-                type="text"
-                value={titleTwo}
-                onChange={event => setTitleNew(event.target.value)}
-            />
-            <button type="submit" className={a.auth__form__button} onClick={handleSubmit}>
-                Войти
-            </button>
-        </form>
+        <form className={a.auth__form}>
+        <input
+          placeholder="Логин"
+          className={a.auth__form__input}
+          type="text"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input
+          placeholder="Пароль"
+          className={a.auth__form__input}
+          type="password"
+          value={titleTwo}
+          onChange={event => setTitleNew(event.target.value)}
+        />
+        <button type="submit" className={a.auth__form__button} onClick={handleSubmit}>
+          Войти
+        </button>
+      </form>
     )
 }
