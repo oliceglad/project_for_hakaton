33e00dbb9be2759eca0ad5d8208f6f1d404137d8
djared <92023728+oliceglad@users.noreply.github.com>
2022-11-04 17:04:41 +0400

    delete auth

--- Autorization/src/main/java/com/tcm/spring/security/configuration/MySecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.tcm.spring.security.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
