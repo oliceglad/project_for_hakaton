bc1b98993a6903ddd8d27a033e527474a62635d3
djared <92023728+oliceglad@users.noreply.github.com>
2022-11-06 00:11:38 +0400

    Delete app directory

--- app/src/Components/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './Header.module.scss';
-
-export const Header = () => {
-    return (
-        <h1 className={s.auth__title}>Сервис по расчету стоимости недвижимости </h1>
-    )
-}
